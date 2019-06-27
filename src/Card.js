7c24100c5c83974fab03e82583990dc7b5bcf457
eneso <e.furkanoguz@gmail.com>
2019-06-26 23:02:17 -0600

    created the interface

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
     return(
-        <div>
-            <img src="https://robohash.org/test" alt="robots"/>
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt="robots" src={`https://robohash.org/${props.id}?200x200"}`}/>
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
             </div>
         </div>
         
     );
 
-}
+} 
 
 export default Card;
